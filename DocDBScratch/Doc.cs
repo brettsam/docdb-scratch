72550ba52b1fc0fe34c99b74b77a0828a40711c1
Brett Samblanet <brettsam@microsoft.com>
2016-03-25 15:45:30 -0700

    adding proj

---
+++ DocDBScratch/Doc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NHClientTest
+{
+    public class Doc
+    {
+        [JsonProperty("_ts")]
+        [JsonConverter(typeof(EpochConverter))]
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ DocDBScratch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Azure.Documents.Client;
+using NHClientTest;
+
+namespace DocDBScratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var docDBclient = new DocumentClient(new Uri("yoururl"), "yourkey");
+
+            var collUri = UriFactory.CreateDocumentCollectionUri("ItemDb", "ItemCollection");
+            var results = docDBclient.CreateDocumentQuery<Doc>(collUri).AsEnumerable().ToList();
+
+            // do whatever you want here to find min, max, count, etc
+        }
+    }
+}
